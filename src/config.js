b8ad8722b5de38367a8cf4b5899d0f3971714ccf
Rodell Warner <rodell.warner@gmail.com>
2020-10-09 14:17:32 -0500

    setting up for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: "$2a$10$Zl6HZxKnaSp07oBb6wSrE.dJ3HFymcrMs4WCZOe37IpaKxeitQH5e",
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
